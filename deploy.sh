936332f0b146e0e57f087348d885f78b3ebc8813
Philip Jägenstedt <philip@foolip.org>
2017-08-25 14:42:44 +0200

    Use Travis to deploy to new server
    This is the same setup as in https://github.com/whatwg/books.

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+if [ "$TRAVIS_BRANCH" != "master" -o "$TRAVIS_PULL_REQUEST" != "false" ]; then
+    echo "Skipping deploy for a pull request"
+else
+    ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+    ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+    ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+    ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+    openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
+    chmod 600 deploy_key
+    eval `ssh-agent -s`
+    ssh-add deploy_key
+    echo "$SERVER $SERVER_PUBLIC_KEY" > known_hosts
+    mkdir $WEB_ROOT
+    mv Overview.whatwg.html $WEB_ROOT/index.html
+    rsync --archive --chmod=D755,F644 --verbose --compress --delete --rsh="ssh -o UserKnownHostsFile=known_hosts" $WEB_ROOT deploy@$SERVER:/var/www/
+fi
